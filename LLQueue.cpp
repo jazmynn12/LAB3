b48360984ccedf1c1410e033584b2edec01afe38
jazmynn12 <jnnnewman@ucdavis.edu>
2024-07-26 18:16:53 -0700

    Update LLQueue.cpp

--- LLQueue.cpp
+++ LLQueue.cpp
@@ -1,27 +1,41 @@
+/** 
+ * Lab Number: 3
+ * Name: Mira Haldar and Jazmyne Newman
+ This program will edit the list depending on the task
+*/
+
 #include "currency.cpp"
 #include "DoublyLinkedList.cpp"
 #pragma once
 class LLQueue : private DoublyLinkedList {
 public:
     LLQueue() : DoublyLinkedList() {}
+/*post : empty queue is created*/
     ~LLQueue() {}; 
+/*post : queue is destroyed and memory is released */
+
+/*pre : a valid Currency object is provided
+ *post : add object to list
+*/
 
     void enqueue( Currency * currency) {
         addCurrency(currency);
     }
-
+/* post : remove object from list*/
     Currency * dequeue() {
         return removeCurrency(0);
     }
-
+/*  pre : queue not empty
+    post : returns 1st node in list*/
     Currency * peekFront() {
         return getCurrency(0);
     }
-
+/*  pre : queue not empty
+    post : returns last node in list*/
     Currency * peekRear() {
         return getEnd()->getData();
     }
-
+/*post : a string representsation of the queue's content is returned*/
     std::string toString() {
         return printList();
     }
